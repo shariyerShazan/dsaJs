878d3465fca5effc480ec4d76b448672fe4dc48e
shariyerShazan <shariyershazan1@gmail.com>
2025-09-11 02:58:54 +0600

    swap and prompt

--- one/dataTypes.js
+++ one/dataTypes.js
@@ -7,7 +7,7 @@ let d = "15"
 
 // addition
 console.log(typeof(a+c)) //output - number
-console.log(a+c)//output - 27
+console.log(a+c) //output - 27
 
 
 // concatination 
@@ -15,3 +15,10 @@ console.log(typeof(a+b)) // output - string
 console.log(a+b) // output - 1212
 
 console.log("in 2025 my age is" + a + c) //output - in 2025 my age is 1215
+
+console.log( a + c + "is my age in 2025 " ) //output - 30 is my age in 2025 
+
+console.log("in 2025 my age is" + ( a + c)) //output - in 2025 my age is 27
+
+console.log("1" + 1) // output - 11
+console.log("1" - 1) // output - 0
---
+++ one/prompt.js
@@ -0,0 +1,23 @@
+let age = prompt("enter your age here")
+console.log(typeof(age)) // output - string
+
+
+//
+let age2 = prompt("Enter your age here") 
+age2 = Number(age2)
+console.log(typeof(age2)) // output - number
+
+
+// 
+let age3 = Number(prompt("Enter your age here"))
+console.log(typeof(age3)) // output - number
+
+
+// 
+let name = Number(prompt("Enter your age here"))
+console.log(typeof(name)) // outoput - string
+
+// "12" can be conver in number 12 .. but "12a" or "shazaa" can't be convert in 12a or Number(shazan){it's always string} ...
+
+
+
