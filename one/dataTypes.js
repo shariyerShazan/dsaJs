35bea28de91117df4e9f5aa4217e28f039a2f344
shariyerShazan <shariyershazan1@gmail.com>
2025-09-11 02:26:24 +0600

    data types

---
+++ one/dataTypes.js
@@ -0,0 +1,17 @@
+// number
+let a = 12 
+let b = "12"
+let c = 15
+let d = "15"
+
+
+// addition
+console.log(typeof(a+c)) //output - number
+console.log(a+c)//output - 27
+
+
+// concatination 
+console.log(typeof(a+b)) // output - string
+console.log(a+b) // output - 1212
+
+console.log("in 2025 my age is" + a + c) //output - in 2025 my age is 1215
--- one/variable.js
+++ one/variable.js
@@ -1,16 +1,25 @@
 // 1
 var a = 5
-console.log(a)// output 5
+console.log(a)// output - 5
 
 // 2
-console.log(b) // output undifine
+console.log(b) // output - undifine
 var b = 5 
 
 
 // 3 
 let c = 5
-console.log(c) // output 5
+console.log(c) // output - 5
 
 // 4 
-console.log(d) // output not define, and give error
+console.log(d) // output - not define, and give error
 let d = 5
+
+
+
+const e = 5
+function one (){
+    const e = 10 
+    console.log(e)// output - 10
+}
+one()
