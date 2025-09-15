4603eb3a18b1b467a84e351fc9b6111e274bf4b2
shariyerShazan <shariyershazan1@gmail.com>
2025-09-16 03:44:33 +0600

    Math..

---
+++ one/math.js
@@ -0,0 +1,10 @@
+console.log(Math.floor(2.88)); // output - 2
+console.log(Math.ceil(2.22)); // output - 3
+console.log(Math.round(2.22)); // output - 2
+console.log(Math.round(2.56)); // output - 3
+
+
+let a = 4532
+console.log(a/10); // output - 453.2
+console.log(Math.floor(a/10)); // output - 453
+console.log(a%10) // output - 2
