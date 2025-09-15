d8b91bc9ce36532bc7c026ea8e5741bf4e9e0803
shariyerShazan <shariyershazan1@gmail.com>
2025-09-16 03:49:51 +0600

    relational oparetor

--- one/math.js
+++ one/math.js
@@ -8,3 +8,9 @@ let a = 4532
 console.log(a/10); // output - 453.2
 console.log(Math.floor(a/10)); // output - 453
 console.log(a%10) // output - 2
+
+
+console.log(Math.random()) // output - any randome number 0.00 -0.99
+console.log(Math.floor(Math.random() * 100)) // output - any randome int number  1 - 100 
+console.log(Math.random() * 100) // output - any randome float number  1 - 100 
+console.log(Math.floor((Math.random() * 100) + 10)) // output - any randome int number  11 - 100 
---
+++ one/relationOp.js
@@ -0,0 +1,28 @@
+console.log(10>5); // output - true
+console.log(5>10); // output - false
+
+
+// (variable = assign a value)
+//  (value == value) (value, type === value,type)
+
+
+console.log(10 == "10"); // output - true
+console.log(10 === "10"); // output - false
+console.log(10 === 10); // output - true
+console.log(10 <= 11); // output - true
+console.log(10 < 10); // output - false
+
+
+// oparetor ||
+console.log((1===1) || (2===2)); // output- true
+console.log((1===1) || (2===3)); // output - true
+
+// oparetor &&
+console.log((1===1) && (2===2)); // output- true
+console.log((1===1) && (2===3)); // output - fales
+
+
+// oparetor !
+console.log(!true); // output - false
+console.log(!false); // output - true
+
