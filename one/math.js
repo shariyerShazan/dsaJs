aecb47e488405b988d99deb7ae9c8245abd62587
shariyerShazan <shariyershazan1@gmail.com>
2025-09-16 04:16:32 +0600

    some math function

--- one/math.js
+++ one/math.js
@@ -14,3 +14,21 @@ console.log(Math.random()) // output - any randome number 0.00 -0.99
 console.log(Math.floor(Math.random() * 100)) // output - any randome int number  1 - 100 
 console.log(Math.random() * 100) // output - any randome float number  1 - 100 
 console.log(Math.floor((Math.random() * 100) + 10)) // output - any randome int number  11 - 100 
+
+
+
+console.log(Math.pow(2, 5)) // output - 32
+console.log(Math.sqrt(121)) // output - 11 
+console.log(Math.cbrt(125)) // output - 5
+
+
+console.log(Math.trunc(12.22)) // output - 12 // ! it's remove value after point
+console.log(Math.trunc(12.92)) // output - 12
+
+
+console.log(Math.abs(-100)) // output - 100 // ! transfer negetive to positive 
+console.log(Math.abs(100)) // output - 100 // ! but not positive to negetive
+
+
+console.log(Math.max(10 , 20 , 100 , 40)) // output - 100 // ! maximun number 
+console.log(Math.min(10 , 20 , 100 , 40)) // output - 10 // ! minimum number 
