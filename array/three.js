dbcfce7eac5fca256b08a6ce70358304875fac51
shariyerShazan <shariyershazan1@gmail.com>
2025-09-19 12:49:15 +0600

    array three

--- array/three.js
+++ array/three.js
@@ -13,3 +13,36 @@ for(let i = 2 ; i < arr.length ; i++){
      }
 }
 console.log(secondMax) // output - 101  
+
+
+
+
+// reverse array 
+let i = 0 ;
+let j = arr.length - 1 ;
+while(i !== j){
+    let temp = arr[i] ;
+    arr[i] = arr[j] ;
+    arr[j] = temp ;
+    i++ 
+    j--
+}
+console.log(arr) // output - [ 102,  93, 101,  59,  39, 49, 102, 102, 102, 102, 11 ]
+
+
+
+
+// 
+let newArr = [ 0, 1 , 1 ,  0 , 1 , 0 , 1 , 0  , 0 , 0 , 1 , 1 , 0 , 1 ,0 , 1];
+let x = 0 ;
+let y = 0 ;
+while(x < newArr.length){
+    if(newArr[x] == 0){
+        let temp = newArr[x] ;
+        newArr[x] = newArr[y] ;
+        newArr[y] = temp ;
+        y++
+    }
+    x++
+}
+console.log(newArr) // output - [  0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1  ]
